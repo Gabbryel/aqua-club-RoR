3ed80a9ff41dc086b9f0677a073d40c1a88a5a49
Gabriel Ursache <gabriel@markov.ro>
2020-06-18 17:18:25 +0300

    mobile done

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -2,3 +2,8 @@
 
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
+Rack::Mime::MIME_TYPES.merge!({
+  ".mp4" => 'video/mp4;codecs="AVC1.4D401E, MP4A.40.2"',
+  ".WebM" => 'video/webm;codecs="vp8,vorbis"',
+  ".ogv" => 'video/ogg'
+})
