163d445e0ed88399f5d7915b958255b6ffe6588f
Gabriel Ursache <gabriel@markov.ro>
2020-06-20 13:20:49 +0300

    news done

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,45 @@
+class ArticlesController < ApplicationController
+    skip_before_action :authenticate_user!, only: %i[index]
+    before_action :set_article, only: %i[ edit destroy]
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.create(article_params)
+        if @article.save
+            redirect_to root_path
+        else
+            render :new
+        end
+    end
+
+    def index
+        @articles = Article.all.order('id DESC')
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        
+    end
+
+    def destroy
+        @article.destroy
+        redirect_to root_path
+    end
+
+    private
+
+    def article_params
+        params.require(:article).permit(:body)
+    end
+
+    def set_article 
+        @article = Article.find(params[:id])
+    end
+    
+end
