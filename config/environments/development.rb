2f4beb0759c7e722a1e3ab029c0bf7fd75961054
Gabriel Ursache <gabriel@markov.ro>
2020-06-21 21:21:56 +0300

    solved compatibility with ios and safari

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-  config.hosts << "748730fb98a4.ngrok.io"
+  config.hosts << "128cb8d5c498.ngrok.io"
   # Do not eager load code on boot.
   config.eager_load = false
 
