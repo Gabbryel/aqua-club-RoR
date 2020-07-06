400a7a94cc6c21bd50733579c47a59df17a79dd4
Gabriel Ursache <gabriel@markov.ro>
2020-07-06 21:34:44 +0300

    about page v1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-  config.hosts << "de7ab5f501c2.ngrok.io"
+  config.hosts << "b4b69ec4ffcc.ngrok.io"
   # Do not eager load code on boot.
   config.eager_load = false
 
